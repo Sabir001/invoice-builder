47ba4ceb6bbb2f9283693f699044f862452010a8
Rahat89 <rahat.feni99@gmail.com>
2020-03-03 16:37:23 +0600

    finished class task

---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components"
+const TestDiv = styled.div`
+   color: red;
+   background: green;
+`;
+const Test = () => {
+   return(
+       <TestDiv> Test page </TestDiv>
+   );
+};
+
+export default Test;
