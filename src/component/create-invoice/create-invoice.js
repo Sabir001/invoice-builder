cf1de49975d062dd786673814a885d46b0694ffe
mahafuz <m.mahfuz.me@gmail.com>
2020-03-10 16:28:11 +0600

    added dynamic items field

--- src/component/create-invoice/create-invoice.js
+++ src/component/create-invoice/create-invoice.js
@@ -1,129 +1,221 @@
 import React, { useState } from "react";
-import ImageUploader from 'react-images-upload';
+import ImageUploader from "react-images-upload";
 import { Link } from "react-router-dom";
 
-import { 
-    Wrapper,
-    Row,
- } from "../../style/common";
+import { Wrapper, Row } from "../../style/common";
 
-import { 
-    HalfWidthLeft,
-    HalfWidthRight,
-    InputField,
-    InputWrapper,
-    InvoiceTitle,
-    InlineInputLabel,
-    InlineInputField,
-    TableInputField,
-    TableMain,
-    TableHead,
-    TableRow,
-    TableTD
+import {
+  HalfWidthLeft,
+  HalfWidthRight,
+  InputField,
+  InputWrapper,
+  InvoiceTitle,
+  InlineInputLabel,
+  InlineInputField,
+  TableInputField,
+  TableMain,
+  TableHead,
+  TableRow,
+  TableTD
 } from "../../style/create-style";
 
 const CreateInvoice = () => {
-    const [logo, setLogo] = useState('wxww');    
-    const [formData, updateFormData] = useState();
-    const [item, setItem] = useState( [ {
-        name: '',
-        quantity: '',
-        rate: '',
-        amount: ''
-    } ] );
+  const [logo, setLogo] = useState("wxww");
+  const [formData, updateFormData] = useState();
 
-    const handleBillFrom = (e) => {
-        updateFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
+  const [items, setItems] = useState([
+    {
+      name: "",
+      quantity: "",
+      rate: "",
+      amount: ""
     }
+  ]);
 
-    const onDrop = (picture) => {
-        setLogo(picture);
-    }
+  const handleBillFrom = e => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
 
-    return <Wrapper>
+  const handleRemoveItem = (e, i) => {
+    e.preventDefault();
 
-        <Row>
-            <HalfWidthLeft>
+    setItems(items.filter((item, index) => index !== i));
+  };
 
-                <InputWrapper>
-                    <ImageUploader
-                        withIcon={false}
-                        withPreview={true}
-                        singleImage={true}
-                        onChange={(picture) => onDrop(picture)}
-                        imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                        maxFileSize={2048}
-                    />
-                </InputWrapper>
-                
-                <InputWrapper>
-                    <InlineInputLabel>Bill From:</InlineInputLabel>
-                    <InputField type="text" name="bill_to" onChange={ (e) => handleBillFrom(e) } placeholder="Who is this Invoice from?" />
-                </InputWrapper>
+  const handleAddItem = e => {
+    e.preventDefault();
 
-                <InputWrapper>
-                    <InlineInputLabel>Bill To:</InlineInputLabel>
-                    <InputField type="text" name="bill_from" onChange={ (e) => handleBillFrom(e) } placeholder="Who is this Invoice to?" />
-                </InputWrapper>
+    const newItem = {
+      name: "",
+      quantity: "",
+      rate: "",
+      amount: ""
+    };
 
-            </HalfWidthLeft>
+    setItems([...items, newItem]);
+  };
 
-            <HalfWidthRight>
+  const handleItemChange = (e, index) => {
+    const newItems = [...items];
+    newItems[index] = {
+      ...items[index],
+      [e.target.name]: e.target.value
+    };
 
-                <InvoiceTitle>Invoice</InvoiceTitle>
+    setItems(newItems);
+  };
 
-                <InputWrapper>
-                    <InlineInputLabel>Invoice No:</InlineInputLabel>
-                    <InlineInputField type="text" name="invoice_no" onChange={ (e) => handleBillFrom(e) } placeholder="ARC0001001" />
-                </InputWrapper>
+  const onDrop = picture => {
+    setLogo(picture);
+  };
 
-                <InputWrapper>
-                    <InlineInputLabel>Date:</InlineInputLabel>
-                    <InlineInputField type="text" name="date" onChange={ (e) => handleBillFrom(e) } placeholder="21/03/2020" />
-                </InputWrapper>
+  return (
+    <Wrapper>
+      <Row>
+        <HalfWidthLeft>
+          <InputWrapper>
+            <ImageUploader
+              withIcon={false}
+              withPreview={true}
+              singleImage={true}
+              onChange={picture => onDrop(picture)}
+              imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+              maxFileSize={2048}
+            />
+          </InputWrapper>
 
-                <InputWrapper>
-                    <InlineInputLabel>Payment Terms:</InlineInputLabel>
-                    <InlineInputField type="text" name="payment_terms" onChange={ (e) => handleBillFrom(e) } placeholder="Payment Terms" />
-                </InputWrapper>
+          <InputWrapper>
+            <InlineInputLabel>Bill From:</InlineInputLabel>
+            <InputField
+              type="text"
+              name="bill_to"
+              onChange={e => handleBillFrom(e)}
+              placeholder="Who is this Invoice from?"
+            />
+          </InputWrapper>
 
-                <InputWrapper>
-                    <InlineInputLabel>Due Date:</InlineInputLabel>
-                    <InlineInputField type="text" name="due_date" onChange={ (e) => handleBillFrom(e) } placeholder="21/03/2020" />
-                </InputWrapper>
+          <InputWrapper>
+            <InlineInputLabel>Bill To:</InlineInputLabel>
+            <InputField
+              type="text"
+              name="bill_from"
+              onChange={e => handleBillFrom(e)}
+              placeholder="Who is this Invoice to?"
+            />
+          </InputWrapper>
+        </HalfWidthLeft>
 
-            </HalfWidthRight>
-        </Row>
+        <HalfWidthRight>
+          <InvoiceTitle>Invoice</InvoiceTitle>
 
-        <Row>
-            <TableMain>
-                { item.map((data) => 
-                    <TableRow>
-                        <TableTD> <TableInputField type="text" value={ data.name } /> </TableTD>
-                        <TableTD> <TableInputField type="text" value={ data.quantity } /> </TableTD>
-                        <TableTD> <TableInputField type="text" value={ data.rate } /> </TableTD>
-                        <TableTD> <TableInputField type="text" value={ data.amount } /> </TableTD>
-                    </TableRow>
-                ) }
-            </TableMain>
-        </Row>
+          <InputWrapper>
+            <InlineInputLabel>Invoice No:</InlineInputLabel>
+            <InlineInputField
+              type="text"
+              name="invoice_no"
+              onChange={e => handleBillFrom(e)}
+              placeholder="ARC0001001"
+            />
+          </InputWrapper>
 
-        <Row>
-            {console.log(logo)}
-            <Link target="_blank" to={{
-                pathname: "/pdf", 
-                state: {
-                    test: "logo"
-                }
-            }}>
-              <span>Preview PDF</span>
-            </Link>
-        </Row>
+          <InputWrapper>
+            <InlineInputLabel>Date:</InlineInputLabel>
+            <InlineInputField
+              type="text"
+              name="date"
+              onChange={e => handleBillFrom(e)}
+              placeholder="21/03/2020"
+            />
+          </InputWrapper>
 
-    </Wrapper>;
-}
+          <InputWrapper>
+            <InlineInputLabel>Payment Terms:</InlineInputLabel>
+            <InlineInputField
+              type="text"
+              name="payment_terms"
+              onChange={e => handleBillFrom(e)}
+              placeholder="Payment Terms"
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InlineInputLabel>Due Date:</InlineInputLabel>
+            <InlineInputField
+              type="text"
+              name="due_date"
+              onChange={e => handleBillFrom(e)}
+              placeholder="21/03/2020"
+            />
+          </InputWrapper>
+        </HalfWidthRight>
+      </Row>
+
+      <Row>
+        <TableMain>
+          {items.map((item, i) => {
+            return (
+              <TableRow key={i}>
+                <TableTD>
+                  {" "}
+                  <TableInputField
+                    name="name"
+                    type="text"
+                    onChange={e => handleItemChange(e, i)}
+                  />
+                </TableTD>
+                <TableTD>
+                  {" "}
+                  <TableInputField
+                    name="quantity"
+                    type="text"
+                    onChange={e => handleItemChange(e, i)}
+                  />
+                </TableTD>
+                <TableTD>
+                  {" "}
+                  <TableInputField
+                    name="rate"
+                    type="text"
+                    onChange={e => handleItemChange(e, i)}
+                  />
+                </TableTD>
+                <TableTD>
+                  {" "}
+                  <TableInputField
+                    name="amount"
+                    type="text"
+                    onChange={e => handleItemChange(e, i)}
+                  />
+                </TableTD>
+                <button onClick={e => handleRemoveItem(e, i)}>X</button>
+              </TableRow>
+            );
+          })}
+          <button onClick={e => handleAddItem(e)}>+ Line Item</button>
+
+          {console.log(items)}
+        </TableMain>
+      </Row>
+
+      <Row>
+        {console.log(logo)}
+        <Link
+          target="_blank"
+          to={{
+            pathname: "/pdf",
+            state: {
+              test: "logo"
+            }
+          }}
+        >
+          <span>Preview PDF</span>
+        </Link>
+      </Row>
+    </Wrapper>
+  );
+};
 
 export default CreateInvoice;
